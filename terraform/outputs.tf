3220d7a8490d633a8c4f427f937cd2974c3c7dd3
José Flores <josefloressv@gmail.com>
2025-01-29 18:07:18 -0600

    Update EKS kubeconfig output to include environment alias and comment out ArgoCD outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,7 +4,7 @@ output "eks_name" {
 }
 
 output "eks_connect" {
-  value = "aws eks --region us-east-1 update-kubeconfig --name ${module.eks.cluster_name}"
+  value = "aws eks --region us-east-1 update-kubeconfig --name ${module.eks.cluster_name} --alias ${local.environment}"
 }
 #endregion
 
@@ -19,11 +19,11 @@ output "vpc_public_subnet_ids" {
 #endregion
 
 # region Helm outputs
-output "argocd_load_balancer_dns" {
-  value = "https://${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].hostname}"
-}
+# output "argocd_load_balancer_dns" {
+#   value = "https://${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].hostname}"
+# }
 
-output "argocd_initial_admin_secret" {
-  value = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath=\"{.data.password}\" | base64 -d"
-}
+# output "argocd_initial_admin_secret" {
+#   value = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath=\"{.data.password}\" | base64 -d"
+# }
 #endregion
