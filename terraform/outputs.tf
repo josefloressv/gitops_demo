3cf504f68fddf5fb5e23d77b9e0fd622d78edaa0
José Flores <josefloressv@gmail.com>
2025-01-29 20:56:36 -0600

    Uncomment ArgoCD output variables in Terraform configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -19,11 +19,11 @@ output "vpc_public_subnet_ids" {
 #endregion
 
 # region Helm outputs
-# output "argocd_load_balancer_dns" {
-#   value = "https://${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].hostname}"
-# }
+output "argocd_load_balancer_dns" {
+  value = "https://${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].hostname}"
+}
 
-# output "argocd_initial_admin_secret" {
-#   value = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath=\"{.data.password}\" | base64 -d"
-# }
+output "argocd_initial_admin_secret" {
+  value = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath=\"{.data.password}\" | base64 -d"
+}
 #endregion
